782a38dc0dd788327793a91c9d32cbcce40ab66e
greatdream55 <jeffrydare912@gmail.com>
2022-10-24 14:16:11 -0700

    add w1d1 afternoon lecture code

---
+++ W1D1_JS_Fundamentals/more.js
@@ -0,0 +1,37 @@
+// const person = {
+//     name: "john",
+//     favFood: "🍕",
+//     age: 37
+// };
+
+// const age = 1337;
+// const {name, age:personAge, favFood} = person;
+// // const personName = person.name;
+// // const personFood = person.favFood;
+// // const personAge = person.age;
+// console.log(name, personAge, favFood);
+
+// //                0      1      2
+// const animals = ["🦁", "🦒", "🦓"];
+
+// //     0  1    2
+// const [, , thirdThing] = animals;
+// console.log(thirdThing)
+
+// ------------ arrow functions ---
+
+console.log(sayHi("john"))
+
+const sayHi = (someOne) => {
+    return "hi " + someOne
+}
+// function sayHi(someOne){
+//     return "hi " + someOne
+// }
+
+
+
+
+
+
+
